a152e4cd38231af860741c085d3db63eef59f96d
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-10-15 14:21:37 +0530

    s

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+	extend: {},
 };
+export const plugins = [require("daisyui")];

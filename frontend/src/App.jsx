5cfa0c558bf02b16bb5bfa7549b4e6c7f73c255c
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-10-16 11:58:18 +0530

    Message Container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 import SignUp from "./pages/signup/SignUp.jsx";
-
 function App(){
   return <div className="p-4 h-screen flex items-center justify-center">
   <Home/>

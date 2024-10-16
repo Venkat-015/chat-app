8df8dbbf07ac6645c6de2412fcfbd56edd9a5149
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-10-16 20:49:55 +0530

    Conversation

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,37 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
-  return <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+  const[username,setUsername]=useState("");
+  const[password,setPassword]=useState("");
+  const{loading,login}=useLogin();
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    await login(username,password);
+  }
+
+  return (
+  <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
          <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
           <h1 className=" text-3xl font-semibold text-center text-gray-300">
             Login
             <span className="text-red-400"> SocketSync</span>
           </h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label className="label p-2">
                 <span className="text-base label-text text-yellow-200">Username</span>
               </label>
-              <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10"/>
+              <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10"
+              value={username} onChange={(e)=>setUsername(e.target.value)}
+              />
             </div>
             <div>
               <label className="label ">
                 <span className="text-base label-text text-yellow-200">Password</span>
               </label>
-              <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10"/>
+              <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10"
+              value={password} onChange={(e)=>setPassword(e.target.value)}
+              />
             </div>
             <Link to='/signup' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
               Don't have an account?
             </Link>
             <div>
-              <button className="btn btn-block btn-sm mt-2 border-green-300">
-                Login
+              <button className="btn btn-block btn-sm mt-2 border-green-300" disabled={loading}>
+                {loading?<span className="loading loading-spinner"/>:
+                "Login"}
               </button>
             </div>
           </form>
          </div>
 
-    </div>;
+    </div>
+  );
 };
 
 export default Login;

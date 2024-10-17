bca5e50e7b5086ceb74a40fb030c29b2071507e6
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-10-17 12:00:00 +0530

    searchbar

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 const SearchInput = () => {
+  const [search,setSearch]=useState("");
+  const {setSelectedConversation}=useConversation();
+  const{conversations}=useGetConversations();
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(!search) return;
+    if(search.length<3){
+      return toast.error('Search String must have atleast 3 characters');
+    }
+    const conversation=conversations.find((c)=>c.fullName.toLowerCase().includes(search.toLocaleLowerCase()));
+    if(conversation){
+      setSelectedConversation(conversation);
+      setSearch("");}
+      else toast.error('No such user found');
+
+  }
   return (
-    <form className="flex items-center gap-2"> 
-        <input type="text" placeholder="Search..." className="input input-bordered rounded-full border-green-400"/>
+    <form onSubmit={handleSubmit}className="flex items-center gap-2"> 
+        <input type="text" placeholder="Search..." className="input input-bordered rounded-full border-green-400"
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+
+        />
         <button type="submit" className="btn btn-circle bg-sky-500 text-white border-yellow-400">
         <IoIosSearch className="w-6 h-5 outline-none"/>
         </button>

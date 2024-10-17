14a0e91ecb1ffbc051283db79b1d4e5484d88abc
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-10-17 13:58:23 +0530

    web sockets

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-const app=express();
+import { app,server } from "./socket/socket.js";
 const PORT=process.env.PORT||5000;
 dotenv.config();
 app.use(express.json());
@@ -17,6 +17,6 @@ app.use("/api/users",userRoutes);
   //  res.send("Hello World!");
 //});
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)});
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -9,6 +9,7 @@ const Message = ({message}) => {
   const profilePic=fromMe?authUser.profilePic:selectedConversation?.profilePic;
   const bubbleBgColor=fromMe?'bg-blue-700':" bg-zinc-950";
   const formattedTime=extractTime(message.createdAt);
+  const shakeClass=message.shouldShake?"shake":"";
   return (
     <div className={`chat ${chatClassName}`}>
      <div className="chat-image avatar ">
@@ -18,7 +19,7 @@ const Message = ({message}) => {
         </div>
 
      </div>
-     <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+     <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>
         {message.message}
      </div>
      <div className="chat-footer opacity-50 text-xs flex-gap-1 items-center text-cyan-50">{formattedTime}</div>
